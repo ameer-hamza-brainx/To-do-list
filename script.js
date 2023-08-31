87ad0dc83207ff8af7aace2975ed0d1659db7df3
ameer-hamza-brainx <ameer.hamza@brainxtech.com>
2023-08-31 13:49:04 +0500

    Added edit functionality

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 let checkbox = document.createElement("input");
                 checkbox.type = "checkbox";
+                checkbox.id = "checkbox";
                 
                 let paragraph = document.createElement("p");
+                paragraph.id = "text";
                 paragraph.innerHTML = document.getElementById('inp').value;
 
                 let symbolsClass = document.createElement("div");
                 symbolsClass.classList.add("symbols");
 
                 let editIcon = document.createElement("i");
-                editIcon.classList.add("fa","fa-edit","edit")
+                editIcon.classList.add("fa","fa-edit","edit");
+                editIcon.id = "editIcon";
 
                 let trashIcon = document.createElement("i");
                 trashIcon.classList.add("fa","fa-trash","del")
+                trashIcon.id = "delIcon"
 
                 symbolsClass.appendChild(editIcon);
                 symbolsClass.appendChild(trashIcon);
@@ -37,4 +41,38 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('inp').value = "";
             }
         });
+        document.getElementById('tasks').addEventListener('click',function(e){
+            // console.log(e.target);
+            if(e.target.id === "delIcon"){
+                e.target.parentElement.parentElement.remove();
+            }
+            else if(e.target.id === "editIcon")
+            {
+                console.log(e.target.parentElement.previousElementSibling.previousElementSibling);
+                let inputElement = e.target.parentElement.previousElementSibling.previousElementSibling;
+                inputElement.type = "text";
+                let para = e.target.parentElement.previousElementSibling;
+                inputElement.value = para.innerHTML;
+                para.innerHTML = "";
+                console.log(inputElement.value);
+                inputElement.addEventListener('keypress',function(e){
+                    if(e.key === "Enter")
+                    {
+                        para.innerHTML = inputElement.value;
+                        console.log(inputElement.value);
+                        inputElement.type = "checkbox";
+                    }
+                });
+            }
+            else if(e.target.id==="checkbox")
+            {
+                const para = e.target.nextElementSibling;
+                if(e.target.checked){
+                    para.classList.add("cut-text");
+                }
+                else{
+                    para.classList.remove("cut-text");
+                }
+            }
+        })
     });
